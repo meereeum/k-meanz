c22d91be1ca6ce152e669b7c22bd4eaa9b0f1814
Miriam Shiffman <meereeum@gmail.com>
2015-10-04 21:09:57 -0400

    Add option to download from flickr API based on keyword

--- glitch_random.py
+++ glitch_random.py
@@ -40,7 +40,8 @@ import re
 import glob
 import code
 import webbrowser
-from bs4 import BeautifulSoup
+import requests
+#from bs4 import BeautifulSoup
 
 ###############################################################################
 ###############################################################################
@@ -52,13 +53,11 @@ class glitch():
 
     MAX_CHUNK = 300
 
-    def __init__(self, path, from_file=True):
+    def __init__(self, path):
         """Given filepath to image, initialize glitchable object"""
-        if from_file:
-            self.data = self.read_from_file(path)
-            self.name = subprocess.check_output("basename {}".format(path), shell=True).strip()
-            # set name of file with call to `basename`
-
+        self.data = self.read_from_file(path)
+        # set name of file with call to `basename`
+        self.name = subprocess.check_output("basename {}".format(path), shell=True).strip()
         self.change_log = []
         self.glitchname = re.sub('(\.[^\.]*)$', r'.glitched\1', self.name) # add '.glitched' before file suffix
 
@@ -95,15 +94,16 @@ class glitch():
 
 
     def _random_chunk(self, max_chunk = MAX_CHUNK):
-        """Return random chunk of image data and tuple of start/endpoints, with optional parameter for max size -
-        i.e. [ str, (int, int) ]"""
+        """Return random chunk of image data and tuple of start/endpoints,
+        with optional parameter for max size - output: [ str, (int, int) ]"""
         splice_origin = random.randint(0, len(self.data))
         splice_end = random.randint(splice_origin, splice_origin + max_chunk)
         return [self.data[splice_origin : splice_end], (splice_origin, splice_end)]
 
 
     def genome_rearrange(self, max_n=5, max_chunk = MAX_CHUNK):
-        """Glitch according to rules of basic genome rearrangement: splice chunk, n times, into another location"""
+        """Glitch according to rules of basic genome rearrangement:
+        splice chunk, n times, into another location"""
         n = random.randint(1, max_n)
         site = random.randint(0, len(self.data))
         [chunk, (a, b)] = self._random_chunk(max_chunk)
@@ -119,42 +119,74 @@ class glitch():
         """Glitch by incrementing all digits in random data chunk by n (mod 10)"""
         n = random.randint(1, max_n)
         [chunk, (a, b)] = self._random_chunk(max_chunk)
-        
-        self.data = self.data[:a] + ''.join(str( (int(x)+n) % 10 ) if x.isdigit() else x for x in chunk) + self.data[b:]
+
+        self.data = self.data[:a] + \
+                    ''.join( str( (int(x)+n) % 10 ) if x.isdigit() else x for x in chunk ) + \
+                    self.data[b:]
 
         self.change_log.append('Digits in chunk of {} char ({} to {}) incremented by {}'\
                             .format(b - a, a, b, n))
 
+
 ###############################################################################
 ###############################################################################
 ###############################################################################
 ###############################################################################
+
+
+def flickr_hit(keywd):
+    """Use keyword to download random image hit from Flickr API and return local path to image"""
+    API_KEY='6fb8c9ee707ff3eb8c610d4bfba9ddaf'
+    # get random images matching given keyword using Flickr's API
+    curled = subprocess.check_output("curl 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={}&text={}&format=json&nojsoncallback=1'".format(API_KEY, keywd), shell = True)
+    # select random hit from among top hits
+    random_hit = random.choice( [ img.split(",") for img in curled.split('{') ][3:] )
+    # use hit data to initialize dictionary of image data and construct image URL
+    d_hit = dict( tuple( txt.strip('"') for txt in elem.split(':')[:2] ) for elem in random_hit if len(elem)>0 )
+    hit_url = 'https://farm{}.staticflickr.com/{}/{}_{}.jpg'\
+              .format( *( d_hit[key] for key in ['farm','server','id','secret'] ) )
+    # download image
+    outfile = outfile_path( PATH_OUT, '{}.jpg'.format(d_hit['id']) )
+    with open(outfile, "w") as file_out:
+        file_out.write( requests.get(hit_url).content )
+    return outfile
+
 
 def outfile_path(path_to_dir, filename):
     """Given directory and desired filename, returns path to outfile that will not
     overwrite existing file/s by appending '_<i>' to filename"""
-    path_to_dir = re.sub('/$', '', path_to_dir) # remove trailing slash, if exists
+    # remove trailing slash, if exists
+    path_to_dir = re.sub('/$', '', path_to_dir)
+
+    # only alter filename if necessary to avoid clobbering preexisting file
     if glob.glob('{}/{}'.format(path_to_dir, filename)):
-        # only alter filename if necessary to avoid clobbering preexisting file
         i = 1
-        filename = re.sub('(\.[^\.]*)$', r'_{}\1'.format(i), filename)  # add '_<i>' before file suffix
-        while glob.glob('{}/{}'.format(path_to_dir, filename)): # continually check for pre-existing file
-            filename = re.sub('_[0-9]*(\.[^\.]*)$', r'_{}\1'.format(i), filename) # update i in filename
-            # N.B. r for raw string to enable back referencing
+        # add '_<i>' before file suffix
+        filename = re.sub('(\.[^\.]*)$', r'_{}\1'.format(i), filename)
+        # continually check for pre-existing file
+        while glob.glob('{}/{}'.format(path_to_dir, filename)):
+            # update i in filename
+            filename = re.sub('_[0-9]*(\.[^\.]*)$', r'_{}\1'.format(i), filename)
+            # ^^ 'r' for raw string to enable back-referencing
             i += 1
+
     return '{}/{}'.format(path_to_dir, filename)
 
+
 ###############################################################################
 ###############################################################################
 ###############################################################################
 ###############################################################################
 
-img_in = '/Users/miriamshiffman/Desktop/Pics/Mir/IMG_0869.jpg'
-path_out = '/Users/miriamshiffman/Downloads/'
+KEY = 'australia'
+#IMG_IN = '/Users/miriamshiffman/Desktop/Pics/Mir/IMG_0869.jpg'
+PATH_OUT = '/Users/miriamshiffman/Downloads/'
+IMG_IN = flickr_hit(KEY)
 
 
 if __name__ == '__main__':
-    x = glitch(img_in)
-    for i in xrange(5):
+    x = glitch(IMG_IN)
+    for i in xrange(3):
         x.digit_increment(max_chunk = 1000, max_n=4)
-    x.write_to_file(path_out)
+        x.genome_rearrange()
+    x.write_to_file(PATH_OUT)
