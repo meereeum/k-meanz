0a912d0a8d86fe604801d0d589331fb3227f372e
Miriam Shiffman <meereeum@gmail.com>
2015-12-18 18:40:12 -0500

    clustering + image generation working

--- k_means_vanilla.py
+++ k_means_vanilla.py
@@ -7,12 +7,13 @@ class kmeans():
         self.img = Image.open(filepath)
         #self.img.show()
         self.k = k
+        self.centroids=None
         # initialize pixel map = m x n array (row by column rather than x & y)
-        pixels = np.array(self.img)
+        self.pixels = np.array(self.img)
 
-        self.m, self.n, cols = pixels.shape
+        self.m, self.n, cols = self.pixels.shape
         if cols > 3:
-            pixels = pixels[:,:,:3]
+            self.pixels = self.pixels[:,:,:3]
 
         self.dims = (self.m,self.n)
         if scale:
@@ -25,28 +26,29 @@ class kmeans():
         idx_arr = np.array(idx_lst).reshape((self.m, self.n, 2))
 
         # 2D array = array of [m, n, r, g, b] arrays
-        self.arr = np.concatenate((idx_arr, pixels), axis=2).\
+        self.arr = np.concatenate((idx_arr, self.pixels), axis=2).\
                    ravel().reshape((self.m*self.n,5))
 
         centroids = random.sample(self.arr, self.k)
-        centroids_history = [centroids[:]]
+        self.centroids_history = [centroids[:]]
 
         for _ in xrange(rounds):
             #self._centroids=self.centroids()
             # TODO: use @property decorator ???
             centroids = self.update_centroids(centroids=centroids)
-            centroids_history.append(centroids)
+            self.centroids_history.append(centroids)
             print 'cluster round {} done!'.format(_)
-        self.generate_image(centroids)
 
 
     #@property
     #def centroids(self):
-        #return self._centroids
+        ##return self._centroids
+        #print 'hi'
+        #return self.centroids_history[-1]
 #
     #@centroids.setter
-    #def centroids(self, rounds):
-        #self._centroids = self.update_centroids()
+    #def centroids(self, value):
+        #self._centroids = self.centroids_history[-1]
 
 
     def update_centroids(self, centroids):
@@ -61,10 +63,8 @@ class kmeans():
         for arr in self.arr:
             # dictionary values = [num points, summed dim1, ..., summed dim 5]
             d_centroids[nearest_centroid(arr)] += np.concatenate([[1.0], arr])
-
         #for k,v in d_centroids.iteritems():
             #print 'centroid {}: {} assigned'.format(k,v[0])
-
         return [ val_arr[1:]/val_arr[0] for val_arr in d_centroids.itervalues() ]
 
 
@@ -73,7 +73,8 @@ class kmeans():
         return np.linalg.norm(v1-v2)
 
 
-    def assign_to_clusters(self, centroids):
+    def cluster(self, centroids):
+        """Cluster image array by assigning pixels to the nearest element among the input list of centroids, and return as dictionary"""
         def nearest_centroid(pixel, metric = self.euclidean_dist):
             """Find np array representing best centroid by minimizing distance, and return as tuple (b/c hashable)"""
             dists = [ (k, metric(pixel,k)) for k in centroids ]
@@ -86,16 +87,21 @@ class kmeans():
         return d_clusters
 
 
-    def generate_image(self, centroids):
-        self.new_arr = np.empty(self.arr.shape, dtype=np.uint8)
-        d_clusters = self.assign_to_clusters(centroids)
+    def generate_image(self, round_idx=-1):
+        """Generate new image by clustering pixels according to given centroids (defaulting to last round of clustering) and assiging centroid RGB to each cluster"""
+        assert round_idx <= len(self.centroids_history)
+        new_arr = np.empty_like(self.pixels, dtype=np.uint8)
+        d_clusters = self.cluster(centroids_history[round_idx])
         for centroid, pixels in d_clusters.iteritems():
-            centroid_rgb = [int(x) for x in centroid[-3:]]
+            centroid_rgb = [int(rgb) for rgb in centroid[-3:]]
             for pixel in pixels:
-                self.new_arr[int(pixel[0]),int(pixel[1])] = centroid_rgb
-        self.new_img = Image.fromarray(self.new_arr)
-        self.new_img.show()
+                new_arr[int(pixel[0]/self.ratio),int(pixel[1]/self.ratio)] \
+                    = centroid_rgb
+        new_img = Image.fromarray(new_arr)
+        new_img.show()
 
 
 if __name__ == "__main__":
-    x = kmeans(filepath='/Users/miriamshiffman/Downloads/536211-78101.jpg')
+    INFILE = '/Users/miriamshiffman/Downloads/536211-78101.jpg'
+    x = kmeans(filepath=INFILE, rounds=5)
+    x.generate_image()
